5b520745878bb817c8b134893c3cfaa0c5456f64
Tanvir <tanvirlaravel@gmail.com>
2024-11-13 12:23:35 +0600

    root route set

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/", [PostController::class, 'index'])->name('posts.index');
+// Route::get();
+// Route::post();
